c747142a1cdccf7d1cc9b2dcc31e2a670396e30b
GiangM9 <giangm9@gmail.com>
2018-06-07 14:01:56 +0700

    dev

--- js/kbackground.js
+++ js/kbackground.js
@@ -1,6 +1,19 @@
-function AddSquare($container) {
+function AddSquare() {
 
 }
-$(function() {
 
+$(function() {
+  var bg = $("#background");
+
+  var row = 30;
+  var col = 20;
+  
+  for ( var i = 0; i < row; ++i ) {
+    var rowid = "row-" + i;
+    bg.append("<div id=" + rowid + ">");
+    var rowdiv = $("#" + rowid);
+    for (var j = 0 ; j < col; ++j) {
+      rowdiv.append("<div class='square'> </div>");
+    }
+  }
 });
